68582a4efb0fc39bf92b847e010c46de90335cf9
jawad Bin Azam <abidibnazam@gmail.com>
2023-08-26 20:58:42 +0600

    typescript object

---
+++ 01INTRO/myobjects.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var user = {
+    name: "Jawad",
+    email: "abid@azam.com",
+    isActive: true,
+};
+function createUser(_a) {
+    var string = _a.name, boolean = _a.isPaid;
+}
+createUser({ name: "Jawad", isPaid: false });
---
+++ 01INTRO/myobjects.ts
@@ -0,0 +1,9 @@
+const user = {
+  name: "Jawad",
+  email: "abid@azam.com",
+  isActive: true,
+};
+
+function createUser({ name: string, isPaid: boolean }) {}
+createUser({ name: "Jawad", isPaid: false });
+export {};
