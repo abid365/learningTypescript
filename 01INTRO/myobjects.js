13af512bc808ecb505a94aa31062f078bb96669b
jawad Bin Azam <abidibnazam@gmail.com>
2023-08-27 00:03:07 +0600

    obj

--- 01INTRO/myobjects.js
+++ 01INTRO/myobjects.js
@@ -8,4 +8,12 @@ var user = {
 function createUser(_a) {
     var string = _a.name, boolean = _a.isPaid;
 }
-createUser({ name: "Jawad", isPaid: false });
+// createUser({ name: "Jawad", isPaid: false , email:"aaaa#aaa"});
+// We can add new property , ex-email cause it does not exist in the object but we can make it with another approach
+var newUser = { name: "Alpha", isPaid: false, email: "lala@jala.com" };
+createUser(newUser);
+// by declaring a object in a variable we can create a new property without facing any error. It is an odd beahaviour of JavaScript
+// case 2
+function createCourse() {
+    return { name: "react-js", price: 450 };
+}
--- 01INTRO/myobjects.ts
+++ 01INTRO/myobjects.ts
@@ -5,5 +5,15 @@ const user = {
 };
 
 function createUser({ name: string, isPaid: boolean }) {}
-createUser({ name: "Jawad", isPaid: false });
+// createUser({ name: "Jawad", isPaid: false , email:"aaaa#aaa"});
+// We can add new property , ex-email cause it does not exist in the object but we can make it with another approach
+let newUser = { name: "Alpha", isPaid: false, email: "lala@jala.com" };
+createUser(newUser);
+// by declaring a object in a variable we can create a new property without facing any error. It is an odd beahaviour of JavaScript
+
+// case 2
+function createCourse(): { name: string; price: number } {
+  return { name: "react-js", price: 450 };
+}
+
 export {};
